3e96a51bd11a27542ec340cf15e24b9111b0e9fe
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-05-12 18:40:04 +0300

    update

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+import json
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QListWidget, QTextEdit,
-                             QHBoxLayout)
+                             QHBoxLayout, QInputDialog, QMessageBox)
 
 # ToDo Добавить возможность добавлять теги и делать быстрый поиск по тегам
 
 WIN_X, WIN_Y = 1200, 800
+filename = "data.json"
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.data = self.read_data()
         self.setWindowTitle("Заметки")
         self.resize(WIN_X, WIN_Y)
         self.create_widgets()
         self.layout_widgets()
         self.connects()
         self.show()
+
+    def read_data(self):
+        try:
+            with open(filename, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except:
+            data = {"Блокнот по умолчанию":
+                {"Заметка по умолчанию":
+                    {
+                        'текст': 'Это самое лучшее приложение для заметок в мире!',
+                        'теги': ['добро', 'инструкция']
+                    }
+                 }
+                    }
+            return data
+
+    def write_data(self):
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(self.data, file, sort_keys=True, ensure_ascii=False)
 
     def create_widgets(self):
         self.lb_notebooks = QLabel("Список блокнотов")
@@ -53,9 +76,45 @@ class MainWindow(QWidget):
 
     def connects(self):
         self.btn_save.clicked.connect(self.note_save)
+        self.btn_add_notebooks.clicked.connect(self.add_notebooks)
+        self.btn_add_notes.clicked.connect(self.add_notes)
 
     def note_save(self):
         pass
+
+    def add_notebooks(self):
+        name_notebooks, ok = QInputDialog.getText(self, "Создать блокнот", "Название блокнота: ")
+        if ok and name_notebooks != "":
+            if name_notebooks not in self.data:
+                self.data[name_notebooks] = {"Заметка по умолчанию":
+                    {
+                        'текст': 'Это самое лучшее приложение для заметок в мире!',
+                        'теги': []
+                    }
+                 }
+                self.lw_notebooks.addItem(name_notebooks)
+            else:
+                QMessageBox.warning(self, "Уведомление", "Блокнот уже существует!")
+        elif ok:
+            QMessageBox.warning(self, "Уведомление", "Название не должно быть пустым!")
+
+    def add_notes(self):
+        if self.lw_notebooks.selectedItems():
+            key = self.lw_notebooks.selectedItems()[0].text()
+            name_note, ok = QInputDialog.getText(self, "Добавить заметку", "Название заметки: ")
+            if ok and name_note != "":
+                if name_note not in self.data[key]:
+                    self.data[key][name_note] = {
+                            'текст': '',
+                            'теги': []
+                        }
+                    self.lw_notes.addItem(name_note)
+                else:
+                    QMessageBox.warning(self, "Уведомление", "Заметка уже существует!")
+            elif ok:
+                QMessageBox.warning(self, "Уведомление", "Название не должно быть пустым!")
+        else:
+            QMessageBox.warning(self, "Уведомление", "Не выбран блокнот для заметки")
 
 
 app = QApplication([])  # Создаем приложение
